762130b6c2debbe65c7757f2f00c0087508dca0b
asura720 <ric.diaza@duocuc.cl>
2026-05-14 14:12:40 -0400

    Api Gateway Cuidapp

---
+++ apigatewaycuidapp/src/main/java/com/cuidapp/apigatewaycuidapp/ApigatewaycuidappApplication.java
@@ -0,0 +1,13 @@
+package com.cuidapp.apigatewaycuidapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewaycuidappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewaycuidappApplication.class, args);
+	}
+
+}
